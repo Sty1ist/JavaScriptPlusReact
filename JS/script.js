2f7be0aa4a94aae263f28f0628d6662a47c68280
Maksym Riabenko <imbalancerm@gmail.com>
2022-12-14 22:46:17 +0200

    update

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let numberOfFilms = prompt('how many movies have you watched?', '1');
+let numberOfFilms = +prompt('how many movies have you watched?', '1');
 
 let personalMovieDB = {
     count: numberOfFilms ? numberOfFilms : 0,
@@ -10,12 +10,30 @@ let personalMovieDB = {
     privat : false,
 }
 
-let questionLastWatched  = prompt('One of the last movies I watched');
-let questionHowMuch  = prompt('How much do you rate it');
-let questionLastWatched1  = prompt('One of the last movies I watched');
-let questionHowMuch1  = prompt('How much do you rate it');
 
-personalMovieDB.movies[questionLastWatched] = questionHowMuch;
-personalMovieDB.movies[questionLastWatched1] = questionHowMuch1;
+for( let i = 0; i < 2; i++ ) {
+
+    let questionLastWatched  = prompt('One of the last movies I watched');
+    let questionHowMuch  = prompt('How much do you rate it');
+
+    if( questionLastWatched != null && questionHowMuch != null && questionLastWatched != '' && questionHowMuch != '' && questionLastWatched.length <= 50) {
+        personalMovieDB.movies[questionLastWatched] = questionHowMuch;
+        console.log('done!')
+    } else {
+        console.log('error');
+        i--;
+    }
+
+}
+
+if(personalMovieDB.count && personalMovieDB.count < 10) {
+    console.log('watched quite a few films');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('you are a classic viewer');
+} else if (personalMovieDB.count >= 30){
+    console.log('you are a movie buff');
+} else {
+    console.log('error!');
+}
 
 console.log('personalMovieDB', personalMovieDB);
