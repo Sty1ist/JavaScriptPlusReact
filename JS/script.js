8605f552bf8cf530eda4388bd13f61741b2d8e39
“Maksym <maksim.riabenko@ribs.pro>
2023-01-04 01:48:42 +0200

    up 29 practice task

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,17 @@
 "use strict";
 
-let numberOfFilms = +prompt('how many movies have you watched?', '1');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('how many movies have you watched?', '1');
+
+    while(numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('how many movies have you watched?', '1');
+    }
+
+};
+
+start();
 
 let personalMovieDB = {
     count: numberOfFilms ? numberOfFilms : 0,
@@ -8,32 +19,56 @@ let personalMovieDB = {
     actors: {},
     genres: [],
     privat : false,
-}
+};
 
+function rememberMyFilms() {
+    for( let i = 0; i < 2; i++ ) {
 
-for( let i = 0; i < 2; i++ ) {
-
-    let questionLastWatched  = prompt('One of the last movies I watched');
-    let questionHowMuch  = prompt('How much do you rate it');
-
-    if( questionLastWatched != null && questionHowMuch != null && questionLastWatched != '' && questionHowMuch != '' && questionLastWatched.length <= 50) {
-        personalMovieDB.movies[questionLastWatched] = questionHowMuch;
-        console.log('done!')
-    } else {
-        console.log('error');
-        i--;
+        let questionLastWatched  = prompt('One of the last movies I watched');
+        let questionHowMuch  = prompt('How much do you rate it');
+    
+        if( questionLastWatched != null && questionHowMuch != null && questionLastWatched != '' && questionHowMuch != '' && questionLastWatched.length <= 50) {
+            personalMovieDB.movies[questionLastWatched] = questionHowMuch;
+            console.log('done!')
+        } else {
+            console.log('error');
+            i--;
+        }
+    
     }
-
 }
 
-if(personalMovieDB.count && personalMovieDB.count < 10) {
-    console.log('watched quite a few films');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log('you are a classic viewer');
-} else if (personalMovieDB.count >= 30){
-    console.log('you are a movie buff');
-} else {
-    console.log('error!');
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if(personalMovieDB.count && personalMovieDB.count < 10) {
+        console.log('watched quite a few films');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('you are a classic viewer');
+    } else if (personalMovieDB.count >= 30){
+        console.log('you are a movie buff');
+    } else {
+        console.log('error!');
+    }
 }
+
+detectPersonalLevel();
+
+function showMyDB(obj) {
+    obj.privat == false ? console.log(obj) : '';
+}
+
+showMyDB(personalMovieDB);
+
+
+function writeYourGenres(obj) {
+    for(let i = 0; i < 3; i++) {
+        let loveGenres = prompt(`Ваш любимый жанр под номером ${i+1}`, 'horror');
+        obj.genres.push(loveGenres);
+    }
+}
+
+writeYourGenres(personalMovieDB) 
+
 
 console.log('personalMovieDB', personalMovieDB);
